1282a12f9c364d79b80915f9a93e156a12675c0d
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2019-11-16 13:12:32 +0200

    Code documentation

--- js/2_events.js
+++ js/2_events.js
@@ -1,13 +1,39 @@
+/**
+ * Name of the product
+ */
+let productName;
+
+/**
+ * PI math constant
+ */
+const PI = 3.141592;
+
+/**
+ * Returns color component if hexadecimal format.
+ */
 let getRandomComponent = () => {
   return Math.ceil(255 * Math.random()).toString(16);
 }
 
+/**
+ * Generates color in CSS format.
+ */
 let getColor = () => { // #ffcc0a
   let r = getRandomComponent(),
       g = getRandomComponent(),
       b = getRandomComponent();
 
   return `#${r}${g}${b}`;
+}
+
+/**
+ * Retruns summ of two numbers.
+ * 
+ * @param {Number} a First number. 
+ * @param {Number} b Second number.
+ */
+let sum = (a, b) => {
+  return a + b;
 }
 
 $(() => {
